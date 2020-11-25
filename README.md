0624ec433e88be87a8fdbaf5d56dbb14f6b4863d
Bunyawat Singchai <bunyawat.s@gmail.com>
2020-11-25 12:00:16 +0700

    init project

---
+++ README.md
@@ -0,0 +1,1 @@
+# packt_flutter_tensorflow_projects
---
+++ android/app/src/main/kotlin/com/example/packt_flutter_tensorflow_projects/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.packt_flutter_tensorflow_projects
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
